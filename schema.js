c93d60770bafd8758ed7aa9e9f64dbaf10e7b291
Shubhendu Bora <82198522+shubhendu0@users.noreply.github.com>
2024-03-11 02:22:22 +0530

    Create schema.js

---
+++ schema.js
@@ -0,0 +1,24 @@
+const mongoose = require("mongoose");
+
+const ProductSchema = new mongoose.Schema(
+    {
+        name: { type: String, required: true },
+        desc: { type: String, required: true },
+        SKU: { type: String, required: true },
+        category_id: { 
+            type: mongoose.Schema.Types.ObjectId, 
+            ref: 'product_category', 
+            required: true,
+        },
+        inventory_id: { 
+            type: mongoose.Schema.Types.ObjectId, 
+            ref: 'product_inventory', 
+            required: true,
+        },
+        price: { type: Number, required: true },
+    },
+    { timestamps: true }
+)
+
+module.exports = mongoose.model("product", ProductSchema);
+
